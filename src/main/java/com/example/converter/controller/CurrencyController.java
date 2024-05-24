2d74e3ed06ee43376ea6e9516df77c3eab188ebd
BohdanTatarchuk <tatarchukbogdan80@gmail.com>
2024-05-24 21:54:28 +0200

    Controllers were made. UserConfig was added. Website files were added.

--- src/main/java/com/example/converter/controller/CurrencyController.java
+++ src/main/java/com/example/converter/controller/CurrencyController.java
@@ -1,10 +1,35 @@
 package com.example.converter.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.converter.model.Currency;
+import com.example.converter.service.Implementation.CurrencyServiceImplementation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping(path = "/api/v1/currencies")
 public class CurrencyController {
-     //TODO
+
+    private final CurrencyServiceImplementation service;
+
+    public CurrencyController(CurrencyServiceImplementation service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public List<Currency> getAllCurrencies() {
+        return service.getAllCurrencies();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Currency> getCurrencyById(@PathVariable String id) {
+        return service.getCurrencyById(id);
+    }
+
+    @DeleteMapping("/")
+    public void deleteCurrency(String id){
+        service.deleteCurrencyById(id);
+    }
 }
