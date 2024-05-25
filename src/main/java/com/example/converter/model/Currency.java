b6e88e712d858ddb26272b07d3c4d4a7e2abb9d7
BohdanTatarchuk <tatarchukbogdan80@gmail.com>
2024-05-25 23:31:10 +0200

    Error in database was fixed

--- src/main/java/com/example/converter/model/Currency.java
+++ src/main/java/com/example/converter/model/Currency.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Currency", schema = "public")
+@Table(name = "currency", schema = "public")
 public class Currency {
 
     @Id
