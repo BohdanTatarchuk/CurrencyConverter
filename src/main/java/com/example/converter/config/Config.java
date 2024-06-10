1eda234f84c8e46f39a42b4037b2d5d0a20454b8
Roman <telishevskyir@gmail.com>
2024-06-10 12:35:07 +0200

    Function updateExchanges was created

--- src/main/java/com/example/converter/config/Config.java
+++ src/main/java/com/example/converter/config/Config.java
@@ -1,16 +1,18 @@
 package com.example.converter.config;
 
+import com.example.converter.repository.ConvertsIntoRepository;
 import com.example.converter.service.Implementation.ConvertsIntoServiceImplementation;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+import com.example.converter.service.RestService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Configuration
 public class Config {
 
-   // @Bean
-   // CommandLineRunner commandLine (ConvertsIntoServiceImplementation service)
-   // {
-   // }
+
 
 }
