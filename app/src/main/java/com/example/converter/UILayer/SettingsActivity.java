7758bbc17d9b98894b459418b743290523d3cdc6
BohdanTatarchuk <tatarchukbogdan80@gmail.com>
2024-06-17 17:18:45 +0200

    Translation files were completed.

--- app/src/main/java/com/example/converter/UILayer/SettingsActivity.java
+++ app/src/main/java/com/example/converter/UILayer/SettingsActivity.java
@@ -1,16 +1,33 @@
 package com.example.converter.UILayer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.QuickContactBadge;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
+import androidx.core.os.LocaleListCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.converter.R;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
+    
+    String selectedLanguage = "en";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,41 @@ public class SettingsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton backButton = findViewById(R.id.settingsBackButton);
+        MaterialAutoCompleteTextView languagesList = findViewById(R.id.settingsLanguagesList);
+
+        Context context = SettingsActivity.this;
+
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+        });
+
+        languagesList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+                        selectedLanguage = "uk";
+                        break;
+                    case 1:
+                        selectedLanguage = "en";
+                        break;
+                    case 2:
+                        selectedLanguage = "de";
+                        break;
+                    case 3:
+                        selectedLanguage = "pl";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                selectedLanguage = "en";
+            }
+        });
+
     }
 }
